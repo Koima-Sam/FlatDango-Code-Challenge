aeb5d7f7d3d9618c57c0979be62ed9ce0b68eb50
Koima-Sam <sam17koima@gmail.com>
2022-06-20 12:30:49 +0300

    Fetch movies from db.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Your code here
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchMovies()
+})
+
+//Create fetch function
+function fetchMovies(){
+    fetch('http://localhost:3000/films')
+    .then(response => response.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie)
+        });
+    })
+}
